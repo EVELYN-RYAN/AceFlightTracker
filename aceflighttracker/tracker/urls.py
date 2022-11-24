b4b7b09bf42f438c30b64c4e4cd00b859a460429
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-11-23 18:00:28 -0700

    Connecting bootstrap static template with Django

--- aceflighttracker/tracker/urls.py
+++ aceflighttracker/tracker/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import indexPageView
+from .views import *
 
 urlpatterns = [
-    path("", indexPageView, name="index")    
+    path("", indexPageView, name="index"),
+    path("punchclock", punchclockPageView, name="punchclock"),
+    path("401", unauthorizedPageView, name='401'),
+    path("404", errorPageView, name='404'),
+    path("500", serverPageView, name='500'),
+    path("login",loginPageView, name='login'),
+    path("password",passwordPageView, name='password'),
+    path("charts", chartsPageView, name='charts'),
+    path("entries", entriesPageView, name='entries'),
+    path("register", registerPageView, name='register')          
 ]                  
             
--- aceflighttracker/tracker/views.py
+++ aceflighttracker/tracker/views.py
@@ -1,5 +1,32 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def indexPageView(request):
-    return HttpResponse("Start of Project")
+    return render(request, 'aceflighttracker/index.html')
+
+def punchclockPageView(request):
+    return render(request, 'aceflighttracker/punchclock.html')
+
+def unauthorizedPageView(request):
+    return render(request,'aceflighttracker/401.html')
+
+def errorPageView(request):
+    return render(request,'aceflighttracker/404.html')
+
+def serverPageView(request):
+    return render(request,'aceflighttracker/500.html')
+
+def loginPageView(request):
+    return render(request,'aceflighttracker/login.html')
+
+def passwordPageView(request):
+    return render(request,'aceflighttracker/password.html')
+
+def registerPageView(request):
+    return render(request,'aceflighttracker/register.html')
+
+def entriesPageView(request):
+    return render(request,'aceflighttracker/entries.html')
+
+def chartsPageView(request):
+    return render(request,'aceflighttracker/charts.html')
