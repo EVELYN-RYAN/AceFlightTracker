91aa9027077f7050d68c31c2626353f0f432f4f8
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-11-27 17:06:24 -0700

    Database and template components work

--- aceflighttracker/tracker/models.py
+++ aceflighttracker/tracker/models.py
@@ -1,3 +1,36 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Flight(models.Model):
+    flightid = models.AutoField(db_column='FlightId', primary_key=True)  # Field name made lowercase.
+    date = models.TextField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
+    days = models.IntegerField(db_column='Days', blank=True, null=True)  # Field name made lowercase.
+    flightnum = models.IntegerField(db_column='FlightNum', blank=True, null=True)  # Field name made lowercase.
+    aircraftid = models.TextField(db_column='AircraftId', blank=True, null=True)  # Field name made lowercase.
+    from_field = models.TextField(db_column='From', blank=True, null=True)  # Field name made lowercase. Field renamed because it was a Python reserved word.
+    to = models.TextField(db_column='To', blank=True, null=True)  # Field name made lowercase.
+    out = models.IntegerField(db_column='Out', blank=True, null=True)  # Field name made lowercase.
+    off = models.IntegerField(db_column='Off', blank=True, null=True)  # Field name made lowercase.
+    on = models.IntegerField(db_column='On', blank=True, null=True)  # Field name made lowercase.
+    in_field = models.IntegerField(db_column='In', blank=True, null=True)  # Field name made lowercase. Field renamed because it was a Python reserved word.
+    total = models.FloatField(db_column='TOTAL', blank=True, null=True)  # Field name made lowercase.
+    night = models.FloatField(db_column='Night', blank=True, null=True)  # Field name made lowercase.
+    imc = models.FloatField(db_column='IMC', blank=True, null=True)  # Field name made lowercase.
+    pilotflying = models.IntegerField(db_column='PilotFlying', blank=True, null=True)  # Field name made lowercase.
+    approachtype = models.TextField(db_column='ApproachType', blank=True, null=True)  # Field name made lowercase.
+    dayt_o = models.IntegerField(db_column='DayT/O', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    dayldg = models.IntegerField(db_column='DayLdg', blank=True, null=True)  # Field name made lowercase.
+    nightt_o = models.IntegerField(db_column='NightT/O', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
+    nightldg = models.IntegerField(db_column='NightLdg', blank=True, null=True)  # Field name made lowercase.
+    remarks = models.TextField(db_column='Remarks', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Flight'
--- aceflighttracker/tracker/views.py
+++ aceflighttracker/tracker/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
+from .models import Flight
 
 # Create your views here.
 def indexPageView(request):
@@ -30,3 +32,12 @@ def entriesPageView(request):
 
 def chartsPageView(request):
     return render(request,'aceflighttracker/charts.html')
+
+class HoursChartView(DetailView):
+    template_name = 'components/hoursGraph.html'
+    model = Flight
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['qs'] = Flight.objects.all()
+        return context
